1ae9894061530e1ed1107f072eb652fe719d6d9a
92jy38 <92jy38@gmail.com>
2024-10-16 15:40:57 +0900

    feat : main 작성

---
+++ src/main/java/com/sparta/schedule/jwt/ScheduleJwtApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.schedule.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleJwtApplication.class, args);
+	}
+
+}
