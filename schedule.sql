512e1c48f1aeac2128763f5667f2b535b72d0e77
92jy38 <92jy38@gmail.com>
2024-10-16 15:38:24 +0900

    feat : sql 작성

---
+++ schedule.sql
@@ -0,0 +1,43 @@
+-- 데이터베이스 생성 및 사용
+CREATE DATABASE IF NOT EXISTS schedule_jwt_db;
+USE schedule_jwt_db;
+
+CREATE TABLE users
+(
+    id          BIGINT          AUTO_INCREMENT PRIMARY KEY,
+    username    VARCHAR(50)     NOT NULL UNIQUE,
+    email       VARCHAR(100)    NOT NULL UNIQUE,
+    created_at  DATETIME        NOT NULL,
+    updated_at  DATETIME        NOT NULL
+
+
+CREATE TABLE schedules
+(
+    id          BIGINT          AUTO_INCREMENT PRIMARY KEY,
+    title       VARCHAR(100)    NOT NULL,
+    ontent      TEXT            NOT NULL,
+    creator_id  BIGINT          NOT NULL,
+    created_at  DATETIME        NOT NULL,
+    updated_at  DATETIME        NOT NULL,
+    FOREIGN KEY (creator_id) REFERENCES users(id) ON DELETE CASCADE
+
+
+CREATE TABLE comments
+(
+    id BIGINT   AUTO_INCREMENT PRIMARY KEY,
+    content     TEXT            NOT NULL,
+    user_id     BIGINT          NOT NULL,
+    schedule_id BIGINT          NOT NULL,
+    created_at  DATETIME        NOT NULL,
+    updated_at  DATETIME        NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (schedule_id) REFERENCES schedules(id) ON DELETE CASCADE
+
+
+CREATE TABLE schedule_user_assignments
+(
+    schedule_id BIGINT          NOT NULL,
+    user_id     BIGINT          NOT NULL,
+    PRIMARY KEY (schedule_id, user_id),
+    FOREIGN KEY (schedule_id) REFERENCES schedules(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
