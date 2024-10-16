3e7419358ef13dbd3ff26ac6bf9699e72d13b47f
92jy38 <92jy38@gmail.com>
2024-10-16 15:37:30 +0900

    feat : 예외처리 작성

---
+++ src/main/java/com/sparta/schedule/jwt/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.sparta.schedule.jwt.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    // NotFoundException에 대한 처리
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<String> handleNotFound(NotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    // BadRequestException에 대한 처리
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<String> handleBadRequest(BadRequestException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    // 기타 예외에 대한 처리
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        return new ResponseEntity<>("서버 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
