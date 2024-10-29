6ef235609ac8b61002c25d27931467946a04d3e5
92jy38 <92jy38@gmail.com>
2024-10-29 22:24:51 +0900

    refactor : 예외처리 방식 변경으로인해 불필요한 클래스 삭제

--- src/main/java/com/sparta/schedule/jwt/exception/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.sparta.schedule.jwt.exception;
-
-public class BadRequestException extends RuntimeException {
-    public BadRequestException(String message) {
-        super(message);
-    }
-}
